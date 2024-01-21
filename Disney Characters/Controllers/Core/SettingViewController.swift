c49048188298f51099956607daa05e41e5b92af1
ethxn-frs <ethanfrancois0@gmail.com>
2024-01-21 14:47:18 +0100

    add settings option
    add some settings option view

--- Disney Characters/Controllers/Core/SettingViewController.swift
+++ Disney Characters/Controllers/Core/SettingViewController.swift
@@ -5,22 +5,42 @@
 //  Created by Etang on 15/01/2024.
 //
 
+import SwiftUI
 import UIKit
 
 
 /// Controller to show app options and settings
 final class SettingViewController: UIViewController {
     
-    private let viewModel = SettingsViewViewModel(
-        cellViewModels: SettingsOption.allCases.compactMap({
-            return SettingsCellViewModel(type: $0)
-        })
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: SettingsView(
+            viewModel: SettingsViewViewModel(
+                cellViewModels: SettingsOption.allCases.compactMap({
+                    return SettingsCellViewModel(type: $0)
+                })
+            )
+        )
     )
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Settings"
+        addSwiftUIController()
     }
-
+    
+    private func addSwiftUIController() {
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
--- Disney Characters/Views/SwiftUI/SettingsView.swift
+++ Disney Characters/Views/SwiftUI/SettingsView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    let viewModel: SettingsViewViewModel
+    
+    init(viewModel: SettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.original)
+                    //.foregroundColor(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
     }
 }
 
-#Preview {
-    SettingsView()
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews : some View {
+        SettingsView(viewModel: .init(cellViewModels: SettingsOption.allCases.compactMap({
+            return SettingsCellViewModel(type: $0)
+        })))
+    }
 }
